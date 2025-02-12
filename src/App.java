cde571884edb7e28ce45624707e7fa11f2c3ef67
NootNoot375 <ctkeating@gmail.com>
2025-02-11 22:35:34 -0500

    Added java files

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int result = 0;
+        int var1 = 10;
+        int var2 = 20;
+        int var3 = 40;
+
+        result = var1 + var2 + var3;
+
+        System.out.println(result);
+
+    }
+}
